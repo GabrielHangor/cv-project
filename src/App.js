3bb1c963342462443916724de3b8a330a4ce94da
HangorG <gaba93@yandex.ru>
2021-04-25 18:02:24 +0300

    basic component structure and layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import Header from './Components/Header';
+import Main from './Components/Main';
 
 class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="container">
+        <Header />
+        <Main />
+      </div>
+    );
   }
 }
 
---
+++ src/Components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <main className="inner-container">
+        <div className="input-form"></div>
+        <div className="preview"></div>
+      </main>
+    );
+  }
+}
+
+export default Main;
